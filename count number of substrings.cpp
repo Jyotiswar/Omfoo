d1cbc059ea0f9a618ff93758be21f695998074e4
JayC_03 <117852605+Jyotiswar@users.noreply.github.com>
2023-10-05 19:57:46 +0530

    Create count number of substrings.cpp

---
+++ count number of substrings.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    long long int countAtMostK(string s, int k) {
+        long long int cnt = 0;
+        int freq[26] = {0};
+        int i = 0, diff = 0;
+
+        for (int j = 0; j < s.size(); ++j) {
+            if (!freq[s[j] - 'a'])
+                ++diff;
+            ++freq[s[j] - 'a'];
+
+            while (diff > k && i <= j) {
+                --freq[s[i] - 'a'];
+                if (!freq[s[i] - 'a'])
+                    --diff;
+                ++i;
+            }
+            cnt += j - i + 1;
+        }
+
+        return cnt;
+    }
+
+    long long int substrCount(string s, int k) {
+        return countAtMostK(s, k) - countAtMostK(s, k - 1);
+    }
+};
+
+int main() {
+    Solution solution;
+    string s = "abcabcabc";
+    int k = 2;
+    
+    long long int result = solution.substrCount(s, k);
+    
+    cout << "Result: " << result << endl;
+    
+    return 0;
+}
